5f02bf6ee2882a2a57e527813b2b3d773be42985
Ahmshere <asus555post@gmail.com>
2024-04-07 18:25:47 +0300

    test

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,7 +1,12 @@
 package pages;
 
+import config.BaseTest;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
 
 public class MainPage extends BasePage{
 
@@ -11,5 +16,18 @@ public class MainPage extends BasePage{
     WebElement aboutTopMenuItem;
     @FindBy(xpath = "//a[contains(text(),'LOGIN')]")
     WebElement loginTopMenuItem;
+    @FindBy(xpath = "//a[contains(text(),'CONTACTS')]")
+    WebElement contactsTopMenuItem;
+    @FindBy(xpath = "//a[contains(text(),'ADD')]")
+    WebElement addTopMenuItem;
+
+public MainPage(WebDriver driver){
+    setDriver(driver);
+    driver.get("https://telranedu.web.app/home");
+    PageFactory.initElements(new AjaxElementLocatorFactory(driver,20), this);
+}
+
+
+
 
 }
