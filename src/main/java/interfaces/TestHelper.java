61eec6e4d4aefaddf24006d417cbbdd93e47ff07
Ahmshere <asus555post@gmail.com>
2024-05-05 18:20:12 +0300

    br branch

--- src/main/java/interfaces/TestHelper.java
+++ src/main/java/interfaces/TestHelper.java
@@ -3,8 +3,7 @@ package interfaces;
 public interface TestHelper {
 
     public static final String XML_FILE_PATH = "src/main/resources/data.xml";
+int rr =88;
 
-    public static final int MY_NUMBER = 1;
-    public static final int MY_NUMBER_2 = 2;
 
 }
