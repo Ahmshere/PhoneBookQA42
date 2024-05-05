ad7c8223615e670dd91f07ef03ce3b9a70f5af9f
Ahmshere <asus555post@gmail.com>
2024-05-05 18:15:43 +0300

    master branch

--- src/main/java/interfaces/TestHelper.java
+++ src/main/java/interfaces/TestHelper.java
@@ -3,5 +3,7 @@ package interfaces;
 public interface TestHelper {
 
     public static final String XML_FILE_PATH = "src/main/resources/data.xml";
+    public static final String XML_FILE_PATH2 = "src/main/resources/data.xml";
+    public static final String XML_FILE_PATH3 = "src/main/resources/data.xml";
 
 }
