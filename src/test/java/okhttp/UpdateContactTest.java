7fd1dc32b0db405634c19b62971ea39d4b6f4540
Ahmshere <asus555post@gmail.com>
2024-05-16 12:34:42 +0300

    15052024

--- src/test/java/okhttp/UpdateContactTest.java
+++ src/test/java/okhttp/UpdateContactTest.java
@@ -13,51 +13,52 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 
 public class UpdateContactTest implements TestHelper {
-    String id;
 
+    String idResult;
     @Test
     public void updateContactPositive() throws IOException {
-        Contact contact = new Contact(
+        Contact contactModel = new Contact(
                 NameAndLastNameGenerator.generateName(),
                 NameAndLastNameGenerator.generateLastName(),
-                EmailGenerator.generateEmail(5, 6, 3),
+                EmailGenerator.generateEmail(7,5,3),
                 PhoneNumberGenerator.generatePhoneNumber(),
-                AddressGenerator.generateAddress(), "blablabla"
-        );
+                AddressGenerator.generateAddress(),"mydesc");
 
-        RequestBody requestBody = RequestBody.create(GSON.toJson(contact), JSON);
+        RequestBody requestBody=RequestBody.create(GSON.toJson(contactModel),JSON);
         Request request = new Request.Builder()
-                .url(BASE_URL + ADD_CONTACT)
+                .url(BASE_URL+ADD_CONTACT)
                 .addHeader(AUTHORIZATION_HEADER, PropertiesReaderXML.getProperty("token", XML_FILE_PATH))
-                .post(requestBody)
-                .build();
+                .post(requestBody).build();
+
         Response response = CLIENT.newCall(request).execute();
         ContactResponseModel contactResponseModel =
                 GSON.fromJson(response.body().string(), ContactResponseModel.class);
-        id = IDExtractor.getId(contactResponseModel.getMessage());
-        System.out.println("ID: "+id);
+        String responseMsg = contactResponseModel.getMessage();
+        System.out.println("RESPONSE MSG: "+responseMsg);
+        idResult = IDExtractor.getId(responseMsg);
 
-        Contact newContact = new Contact(
-                id,
-                contact.getName(),
-                contact.getLastName(),
-                contact.getEmail(),
-                contact.getPhone(),
-                contact.getAddress(),
-                contact.getDescription());
-        RequestBody updatedRequestBody = RequestBody.create(GSON.toJson(newContact), JSON);
-        Request updateRequest = new Request.Builder()
-                .url(BASE_URL+UPDATE_CONTACTS)
+        Contact createdModel = new Contact(
+                idResult,
+                contactModel.getName(),
+                contactModel.getLastName(),
+                contactModel.getEmail(),
+                contactModel.getPhone(),
+                contactModel.getAddress(),
+                contactModel.getDescription() );
+        RequestBody updaterequestBody=RequestBody.create(GSON.toJson(createdModel),JSON);
+        Request updaterequest = new Request.Builder()
+                .url(BASE_URL+ADD_CONTACT)
                 .addHeader(AUTHORIZATION_HEADER, PropertiesReaderXML.getProperty("token", XML_FILE_PATH))
-                .put(updatedRequestBody).build();
-        Response updatedResponse = CLIENT.newCall(updateRequest).execute();
+                .put(updaterequestBody).build();
 
-       ContactResponseModel contactResponseModel1 =
-               GSON.fromJson(updatedResponse.body().string(), ContactResponseModel.class);
-        Assert.assertTrue(updatedResponse.isSuccessful());
-
-
+        Response updateresponse = CLIENT.newCall(updaterequest).execute();
+        ContactResponseModel contactResponseModel1 =
+                GSON.fromJson(updateresponse.body().string(), ContactResponseModel.class);
+        System.out.println(contactResponseModel1.getMessage());
+        Assert.assertTrue(updateresponse.isSuccessful());
     }
+
+
 
 
 }
